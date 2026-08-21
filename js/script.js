86a6f2ba4f6cdbfe07e77ef9df9c5480afa3dabd
RahulNikam910 <rahulnikam910@gmail.com>
2026-08-22 00:57:05 +0530

    Project

--- js/script.js
+++ js/script.js
@@ -1,38 +1,44 @@
 function validate() {
     const name = document.getElementById("name").value.trim();
-
     if (name == "") {
-        alert("Please enter your name");
+        document.getElementById("nameError").innerHTML = "Please enter your name";
         return false;
+    } else {
+        document.getElementById("nameError").innerHTML = "";
     }
 
     const email = document.getElementById("email").value.trim();
-
     if (email == "") {
-        alert("Please enter your email");
+        document.getElementById("emailError").innerHTML = "Please enter your Email ID";
         return false;
+    } else {
+        document.getElementById("emailError").innerHTML = "";
     }
 
     const phone = document.getElementById("phone").value.trim();
-
     if (phone == "") {
-        alert("Please enter your phone number");
+        document.getElementById("phoneError").innerHTML = "Please enter your Mobile Number";
         return false;
+    } else {
+        document.getElementById("phoneError").innerHTML = "";
     }
 
     const subject = document.getElementById("subject").value.trim();
-
     if (subject == "") {
-        alert("Please enter a subject");
+        document.getElementById("subjectError").innerHTML = "Enter Your subject";
         return false;
+    } else {
+        document.getElementById("subjectError").innerHTML = "";
     }
 
     const message = document.getElementById("message").value.trim();
-
     if (message == "") {
-        alert("Please enter your message");
+        document.getElementById("messageError").innerHTML = "Enter your message";
         return false;
+    } else {
+        document.getElementById("messageError").innerHTML = "";
     }
 
-    return true;
+    document.getElementById("successMessage").innerHTML = "✓ Your message has been submitted successfully!";
+    return false;
 }
